8b17c73a592665002a46a85ca666c72a10465f80
Nickita Lashch <holbootla@gmail.com>
2021-03-14 19:13:08 +0300

    add sounds to keys, add playing sounds by mouse moving, add fullscreen

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -4,6 +4,10 @@ const button = buttonContainer.querySelectorAll('.btn')
 const piano = document.querySelector('.piano')
 const pianoKeys = piano.querySelectorAll('.piano-key')
 
+const fullScreenButton = document.querySelector('.fullscreen')
+
+let isPianoKeyClicked = false
+
 buttonContainer.addEventListener('click', (event) => {
   if (!event.target.classList.contains('btn-active')) {
     
@@ -16,3 +20,97 @@ buttonContainer.addEventListener('click', (event) => {
     })
   }
 })
+
+const playAudioByClick = (event) => {
+  if (event.target.classList.contains('piano-key')) {
+
+    isPianoKeyClicked = true
+
+    event.target.classList.add('piano-key-active', 'piano-key-active-pseudo')
+
+    const audio = new Audio()
+    audio.src = `assets/audio/${event.target.dataset.note}.mp3`
+    audio.currentTime = 0
+    audio.play()
+
+    event.target.addEventListener('mouseup', () => {
+      if (event.target.classList.contains('piano-key-active', 'piano-key-active-pseudo'))
+      event.target.classList.remove('piano-key-active', 'piano-key-active-pseudo')
+      isPianoKeyClicked = false
+    })
+
+    event.target.addEventListener('mouseout', () => {
+      if (event.target.classList.contains('piano-key-active', 'piano-key-active-pseudo'))
+      event.target.classList.remove('piano-key-active', 'piano-key-active-pseudo')
+    })
+  }
+}
+
+const playAudioByOver = (event) => {
+  if (event.target.classList.contains('piano-key') && isPianoKeyClicked === true) {
+
+    event.target.classList.add('piano-key-active', 'piano-key-active-pseudo')
+
+    const audio = new Audio()
+    audio.src = `assets/audio/${event.target.dataset.note}.mp3`
+    audio.currentTime = 0
+    audio.play()
+
+    document.addEventListener('mouseup', () => {
+      if (event.target.classList.contains('piano-key-active', 'piano-key-active-pseudo'))
+      event.target.classList.remove('piano-key-active', 'piano-key-active-pseudo')
+      isPianoKeyClicked = false
+      piano.removeEventListener('mouseover', (event) => playAudioByOver(event))
+    })
+
+    event.target.addEventListener('mouseout', () => {
+      if (event.target.classList.contains('piano-key-active', 'piano-key-active-pseudo'))
+      event.target.classList.remove('piano-key-active', 'piano-key-active-pseudo')
+    })
+  }
+}
+
+piano.addEventListener('mousedown', (event) => playAudioByClick(event))
+
+piano.addEventListener('mouseover', (event) => playAudioByOver(event))
+
+const playAudioByKeypress = (event) => {
+
+  if (event.repeat) {
+    console.log('repeat')
+    return
+  }
+
+  pianoKeys.forEach((el) => {
+
+    if (event.code === `Key${el.dataset.letter}`) {
+
+      el.classList.add('piano-key-active', 'piano-key-active-pseudo')
+
+      const audio = new Audio()
+      audio.src = `assets/audio/${el.dataset.note}.mp3`
+      audio.currentTime = 0
+      audio.play()
+
+      event.target.addEventListener('keyup', (event) => {
+        if (el.classList.contains('piano-key-active', 'piano-key-active-pseudo') && event.code === `Key${el.dataset.letter}`)
+        el.classList.remove('piano-key-active', 'piano-key-active-pseudo')
+      })
+    }
+  })
+
+}
+
+document.addEventListener('keydown', (event) => playAudioByKeypress(event))
+
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+}
+
+fullScreenButton.addEventListener('click', () => {toggleFullScreen()})
