3db004cc017283f46d80563cb33c6a6473bd7fa2
Nickita Lashch <holbootla@gmail.com>
2021-03-24 20:54:16 +0300

    feat: add next image loading

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,6 +1,9 @@
 const filters = document.querySelector('.filters')
 const buttons = document.querySelector('.btn-container')
 const fullScreenButton = document.querySelector('.fullscreen')
+const image = document.querySelector('img')
+
+let numberOfImage = 1
 
 const inputHandler = (event) => {
 
@@ -25,12 +28,46 @@ const resetFilters = () => {
     })
 }
 
+const loadNextImage = () => {
+  const date = new Date()
+  const hours = date.getHours()
+
+  let numberOfImageString
+
+  if (numberOfImage < 10) {
+    numberOfImageString = `0${numberOfImage}`
+  } else {
+    numberOfImageString = `${numberOfImage}`
+  }
+
+  if (hours < 6) {
+    image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/night/${numberOfImageString}.jpg`
+  } else if (hours < 12) {
+    image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/morning/${numberOfImageString}.jpg`
+  } else if (hours < 18) {
+    image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/${numberOfImageString}.jpg`
+  } else if (hours < 24) {
+    image.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/${numberOfImageString}.jpg`
+  }
+
+  numberOfImage += 1
+
+  if (numberOfImage > 20) {
+    numberOfImage = 1
+  }
+
+}
+
 const buttonHandler = (event) => {
 
   if (event.target.classList.contains('btn-reset')) {
     resetFilters()
   }
 
+  if (event.target.classList.contains('btn-next')) {
+    loadNextImage()
+  }
+  
 }
 
 buttons.addEventListener('click', (event) => buttonHandler(event))
@@ -49,11 +86,6 @@ window.addEventListener('mouseup', () => {
     }
   }
 )
-
-
-
-
-
 
 const toggleFullScreen = ()=>{
   if (!document.fullscreenElement) {
