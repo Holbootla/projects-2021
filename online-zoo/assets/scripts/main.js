78fb70706bf6205e681d9d58f5780d9dba00dc81
Nickita Lashch <holbootla@gmail.com>
2021-04-26 22:47:30 +0300

    feat: add css variables, add burger menu

---
+++ online-zoo/assets/scripts/main.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.querySelector('.burger-icon')
+const burgerBtnExit = document.querySelector('.burger-icon-exit')
+
+burgerBtn.addEventListener('click', function() {
+  document.querySelector('.navigation-burger').classList.remove('navigation-burger_hidden')
+  document.body.classList.add('overflow-hidden')
+})
+
+burgerBtnExit.addEventListener('click', function() {
+  document.querySelector('.navigation-burger').classList.add('navigation-burger_hidden')
+  document.body.classList.remove('overflow-hidden')
+})
