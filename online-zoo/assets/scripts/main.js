b06811cf4ddedc3e68d82a00502b7bcac5130cbc
Nickita Lashch <holbootla@gmail.com>
2021-04-27 21:49:50 +0300

    feat: add carousel effect to top section

--- online-zoo/assets/scripts/main.js
+++ online-zoo/assets/scripts/main.js
@@ -1,3 +1,5 @@
+// Burger-menu
+
 const burgerBtn = document.querySelector('.burger-icon')
 const burgerBtnExit = document.querySelector('.burger-icon-exit')
 
@@ -11,6 +13,8 @@ burgerBtnExit.addEventListener('click', function() {
   document.body.classList.remove('overflow-hidden')
 })
 
+// Color theme switcher
+
 const switcher = (document.querySelector('.switch')).querySelector('input[type="checkbox"]')
 
 const toggleColorTheme = (e) => {
@@ -22,3 +26,44 @@ const toggleColorTheme = (e) => {
 }
 
 switcher.addEventListener('change', toggleColorTheme)
+
+// Carousel at main page
+
+const carouselControls = document.querySelector('.wrapper-top__slider-container').querySelector('input[type=range]')
+const carouselControlsText = document.querySelector('.wrapper-top__slider-container').querySelector('.slide-number-current')
+const carouselSlides = document.querySelector('.wrapper-top__slider-container').querySelectorAll('.wrapper-top__slider-container__slider__slide')
+
+const moveCarousel = () => {
+  carouselControls.defaultValue = carouselControls.value
+  carouselControlsText.textContent = `0${carouselControls.defaultValue}/`
+  carouselSlides.forEach((el) => {
+    if (el.classList.contains('wrapper-top__slider-container__slider__slide_active')) {
+      el.classList.remove('wrapper-top__slider-container__slider__slide_active')
+    }
+  })
+  carouselSlides[carouselControls.defaultValue-1].classList.add('wrapper-top__slider-container__slider__slide_active')
+  carouselSlides.forEach((el) => {
+    el.style.transform = `translateX(${-(carouselControls.defaultValue - 2) * (el.offsetWidth + (parseInt(getComputedStyle(el).marginRight) * 2))}px)`
+  })
+}
+
+carouselControls.addEventListener('input', moveCarousel)
+
+carouselSlides.forEach((el) => {
+  el.addEventListener('click', function () {
+    let isNextSiblingActive
+    if (el.nextElementSibling) {
+      isNextSiblingActive = el.nextElementSibling.classList.contains('wrapper-top__slider-container__slider__slide_active')
+    } else {
+      isNextSiblingActive = 0
+    }    
+    if (!(el.classList.contains('wrapper-top__slider-container__slider__slide_active')) && !(isNextSiblingActive)) {
+      carouselControls.value = Number(carouselControls.defaultValue) + 1
+      moveCarousel()
+    }
+    if (!(el.classList.contains('wrapper-top__slider-container__slider__slide_active')) && isNextSiblingActive) {
+      carouselControls.value = Number(carouselControls.defaultValue) - 1
+      moveCarousel()
+    }
+  })
+})
