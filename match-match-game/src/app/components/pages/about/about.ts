96246c46299450f4221e51248617e7f8ab396621
Nickita Lashch <holbootla@gmail.com>
2021-05-31 21:49:58 +0300

    feat: change order of rules at about page

--- match-match-game/src/app/components/pages/about/about.ts
+++ match-match-game/src/app/components/pages/about/about.ts
@@ -14,20 +14,20 @@ export default class About {
       'beforeend',
       `
       <div class="rules__rule grid-item-1">
-            <div class="rules__rule__number">1</div>
-            <p class="rules__rule__text">Register new player in game.</p>
-          </div>
-          <img src="image-register.png" alt="" class="rules__img grid-item-2">
-          <div class="rules__rule grid-item-3">
-            <div class="rules__rule__number">2</div>
-            <p class="rules__rule__text">Configure your game settings.</p>
-          </div>
-          <img src="image-settings.png" alt="" class="rules__img grid-item-4">
-          <div class="rules__rule grid-item-5">
-            <div class="rules__rule__number">3</div>
-            <p class="rules__rule__text">Start you new game! Remember card positions and match it before times up.</p>
-          </div>
-          <img src="image-game.png" alt="" class="rules__img grid-item-6">
+        <div class="rules__rule__number">1</div>
+        <p class="rules__rule__text">Configure your game settings.</p>
+      </div>
+      <img src="image-settings.png" alt="" class="rules__img grid-item-2">
+      <div class="rules__rule grid-item-3">
+        <div class="rules__rule__number">2</div>
+        <p class="rules__rule__text">Start you new game! Remember card positions and match it before times up.</p>
+      </div>
+      <img src="image-game.png" alt="" class="rules__img grid-item-4">
+      <div class="rules__rule grid-item-5">
+        <div class="rules__rule__number">3</div>
+        <p class="rules__rule__text">Register new score.</p>
+      </div>
+      <img src="image-register.png" alt="" class="rules__img grid-item-6">
       `
     );
     return this.about;
