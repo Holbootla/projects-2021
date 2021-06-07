4904ce865676bd9babd33b3acf81607e45743589
Nickita Lashch <holbootla@gmail.com>
2021-06-07 20:13:34 +0300

    feat: add title for garage page

--- async-race/src/app/main/garage/garage.ts
+++ async-race/src/app/main/garage/garage.ts
@@ -1,3 +1,4 @@
+import { getCars } from '../../api/api';
 import Controls from './controls';
 
 export default class Garage {
@@ -5,14 +6,24 @@ export default class Garage {
 
   controls: HTMLDivElement;
 
+  title: HTMLDivElement;
+
   constructor() {
     this.garage = document.createElement('div');
     this.garage.classList.add('garage');
     this.controls = new Controls().render();
+    this.title = document.createElement('div');
+    this.title.classList.add('title');
   }
 
   render(): HTMLDivElement {
+    (async () => {
+      const { carsCount } = await getCars();
+      this.title.innerText = `Garage (${carsCount})`;
+    })();
+
     this.garage.appendChild(this.controls);
+    this.garage.appendChild(this.title);
     return this.garage;
   }
 }
