70f4c9371190e2ab82e6833f6c0f2f8e0f4fbf22
Nickita Lashch <holbootla@gmail.com>
2021-07-12 21:41:30 +0300

    feat: add cors

--- english-for-kids-admin-panel/src/app.ts
+++ english-for-kids-admin-panel/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import categoriesRouter from './category/router';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('English for kids admin panel');
