f81a3b2553a207b18e37cce561117968438fe35e
Nickita Lashch <holbootla@gmail.com>
2021-07-13 21:31:29 +0300

    feat: add image property for category

--- english-for-kids-admin-panel/src/category/store.ts
+++ english-for-kids-admin-panel/src/category/store.ts
@@ -3,27 +3,35 @@ import { Category } from './category';
 const categories: Category[] = [
   {
     category: 'Action',
+    image: 'images/category/action.jpg',
   },
   {
     category: 'Animals',
+    image: 'images/category/animals.jpg',
   },
   {
     category: 'Clothes',
+    image: 'images/category/clothes.jpg',
   },
   {
     category: 'Emotions',
+    image: 'images/category/emotions.jpg',
   },
   {
     category: 'Food',
+    image: 'images/category/food.jpg',
   },
   {
     category: 'Family',
+    image: 'images/category/family.jpg',
   },
   {
     category: 'Transport',
+    image: 'images/category/transport.jpg',
   },
   {
     category: 'Sports',
+    image: 'images/category/sports.jpg',
   },
 ];
 
